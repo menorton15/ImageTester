cda305c83908139a1cb32862e048df335d929dd2
Josh Morris <mor18049@byui.edu>
2019-06-18 19:45:49 -0700

    Added comments for saving user preferences
    Removed old comments and extra import statements in main

--- app/src/main/java/com/example/imagetester/MainActivity.java
+++ app/src/main/java/com/example/imagetester/MainActivity.java
@@ -1,24 +1,51 @@
 package com.example.imagetester;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import com.google.gson.Gson;
 
 
 public class MainActivity extends AppCompatActivity {
-    //Comment from Wendy new
-    //Comment from Josh
-    //Comment from Mark
 
     ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        /*
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        String cartJson = sharedPref.getString("CART", "");
+        String currentVehicleJson = sharedPref.getString("CURRENT_TAB", "");
+        String currentTabString = sharedPref.getString("CURRENT_VEHICLE", "");
 
-    // Merge conflicts galore and more.
+        Gson gson = new Gson();
+        myCartObject cart = gson.fromJson(cartJson, myCartObject.class);
+        myVehicleObject currentVehicle = gson.fromJson(currentVehicleJson, myVehicleObject.class);
+        */
+
+    }
+    /*
+    protected void onPause() {
+
+
+        Gson gson = new Gson();
+        String cartJson = gson.toJson(myCartObject);
+        String currentVehicleJson = gson.toJson(myVehicleObject);
+
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("CART", cartJson);
+        editor.putString("CURRENT_VEHICLE", currentVehicleJson);
+        editor.putString("CURRENT_TAB", currentTabString);
+
+        editor.apply();
+
+    }*/
 
 }
