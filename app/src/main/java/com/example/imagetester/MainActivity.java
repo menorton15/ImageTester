afca7092df2196bf0d49d1d48c8bfc39ad8dd0ed
Josh Morris <mor18049@byui.edu>
2019-06-18 18:45:35 -0700

    Added Branch for Josh

--- app/src/main/java/com/example/imagetester/MainActivity.java
+++ app/src/main/java/com/example/imagetester/MainActivity.java
@@ -1,30 +1,9 @@
 package com.example.imagetester;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.ColorFilter;
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
-import android.graphics.LightingColorFilter;
-import android.graphics.Paint;
-import android.graphics.Point;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffColorFilter;
-import android.graphics.drawable.BitmapDrawable;
-import android.support.v4.content.ContextCompat;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
-import android.util.Log;
-import android.view.Display;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
